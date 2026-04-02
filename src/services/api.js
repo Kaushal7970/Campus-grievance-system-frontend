0d3e6321b5eeef5c6e79ef7638070a4115b3f13c
Kaushal7970 <2410030456@klh.edu.in>
2026-04-02 23:43:52 +0530

    fix location error for render build

--- src/services/api.js
+++ src/services/api.js
@@ -1,43 +1,8 @@
-import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-import API_URL from "../api";
-import { safeClear, safeGetItem } from "./storage";
+export const getApiUrl = () => API_URL;
 
-const baseURL = `${API_URL.replace(/\/$/, "")}/api`;
-
-const client = axios.create({ baseURL });
-
-client.interceptors.request.use((req) => {
-	const token = safeGetItem("token");
-	if (token) {
-		if (req.headers && typeof req.headers.set === "function") {
-			req.headers.set("Authorization", `Bearer ${token}`);
-		} else {
-			req.headers = req.headers || {};
-			req.headers.Authorization = `Bearer ${token}`;
-		}
-	}
-	return req;
-});
-
-client.interceptors.response.use(
-	(res) => res,
-	(err) => {
-		if (err?.response?.status === 401) {
-			safeClear();
-			if (typeof location !== "undefined") {
-				location.href = "/";
-			}
-		}
-		return Promise.reject(err);
-	}
-);
-
-const API = {
-	get: (url, config) => client.get(url, config),
-	post: (url, data, config) => client.post(url, data, config),
-	put: (url, data, config) => client.put(url, data, config),
-	delete: (url, config) => client.delete(url, config)
+export const fetchData = async (endpoint) => {
+  const response = await fetch(`${API_URL}/${endpoint}`);
+  return response.json();
 };
-
-export default API;
