963824ce1502777343cc23bd32d68194e85c8a09
Kaushal7970 <2410030456@klh.edu.in>
2026-04-02 23:14:31 +0530

    fix globalThis error and use env api url

--- src/api.js
+++ src/api.js
@@ -1,64 +1,9 @@
-import { safeClear, safeGetItem } from "./services/storage";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-const apiBaseUrl = (import.meta.env.VITE_API_URL || "").replace(/\/$/, "");
-const baseURL = apiBaseUrl ? `${apiBaseUrl}/api` : "/api";
-
-let clientPromise;
-
-async function getClient() {
-  if (clientPromise) return clientPromise;
-
-  clientPromise = (async () => {
-    const axiosMod = await import("axios");
-    const axiosClient = axiosMod?.default || axiosMod;
-
-    if (!axiosClient || typeof axiosClient.create !== "function") {
-      throw new Error("Axios failed to initialize");
-    }
-
-    const instance = axiosClient.create({ baseURL });
-
-    instance.interceptors.request.use((req) => {
-      const token = safeGetItem("token");
-      if (token) {
-        if (req.headers && typeof req.headers.set === "function") {
-          req.headers.set("Authorization", `Bearer ${token}`);
-        } else {
-          req.headers = req.headers || {};
-          req.headers.Authorization = `Bearer ${token}`;
-        }
-      }
-      return req;
-    });
-
-    instance.interceptors.response.use(
-      (res) => res,
-      (err) => {
-        const status = err?.response?.status;
-        if (status === 401) {
-          safeClear();
-          if (globalThis.window?.location) {
-            globalThis.window.location.href = "/";
-          }
-        }
-        return Promise.reject(err);
-      }
-    );
-
-    return instance;
-  })().catch((err) => {
-    clientPromise = undefined;
-    throw err;
-  });
-
-  return clientPromise;
-}
-
-const API = {
-  get: (url, config) => getClient().then((c) => c.get(url, config)),
-  post: (url, data, config) => getClient().then((c) => c.post(url, data, config)),
-  put: (url, data, config) => getClient().then((c) => c.put(url, data, config)),
-  delete: (url, config) => getClient().then((c) => c.delete(url, config))
+export const fetchData = async (endpoint) => {
+  const cleanEndpoint = String(endpoint || "").replace(/^\/+/, "");
+  const res = await fetch(`${API_URL}/${cleanEndpoint}`);
+  return res.json();
 };
 
-export default API;
+export default API_URL;
--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,43 @@
-export { default } from "../api";
+import axios from "axios";
+
+import API_URL from "../api";
+import { safeClear, safeGetItem } from "./storage";
+
+const baseURL = `${API_URL.replace(/\/$/, "")}/api`;
+
+const client = axios.create({ baseURL });
+
+client.interceptors.request.use((req) => {
+	const token = safeGetItem("token");
+	if (token) {
+		if (req.headers && typeof req.headers.set === "function") {
+			req.headers.set("Authorization", `Bearer ${token}`);
+		} else {
+			req.headers = req.headers || {};
+			req.headers.Authorization = `Bearer ${token}`;
+		}
+	}
+	return req;
+});
+
+client.interceptors.response.use(
+	(res) => res,
+	(err) => {
+		if (err?.response?.status === 401) {
+			safeClear();
+			if (typeof location !== "undefined") {
+				location.href = "/";
+			}
+		}
+		return Promise.reject(err);
+	}
+);
+
+const API = {
+	get: (url, config) => client.get(url, config),
+	post: (url, data, config) => client.post(url, data, config),
+	put: (url, data, config) => client.put(url, data, config),
+	delete: (url, config) => client.delete(url, config)
+};
+
+export default API;
