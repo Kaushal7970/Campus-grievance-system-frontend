09207c74cc94848a96feb94048da8b63671c839e
Kaushal7970 <2410030456@klh.edu.in>
2026-04-03 00:59:05 +0530

    update frontend (api + token + fixes)

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+import { safeGetItem } from "./storage";
 
+const rootUrl = (process.env.REACT_APP_API_URL || "http://localhost:5000").replace(/\/$/, "");
 const API = axios.create({
-	baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+	baseURL: `${rootUrl}/api`,
+});
+
+API.interceptors.request.use((config) => {
+	const token = safeGetItem("token");
+	if (token) {
+		config.headers = config.headers || {};
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+	return config;
 });
 
 export default API;
